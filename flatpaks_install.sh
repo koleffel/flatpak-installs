5b008491dff9da68c9d7a2ebe64972d6c20af278
Koleffel <biserster@gmail.com>
2024-07-17 05:24:52 +0300

    modified:   flatpaks_install.sh         Added More Apps That I like         Sorted separated Browsers and Gaming Apps

--- flatpaks_install.sh
+++ flatpaks_install.sh
@@ -22,30 +22,8 @@ flatpak install flathub org.qbittorrent.qBittorrent -y
 
 flatpak install flathub org.onlyoffice.desktopeditors -y
 
-
-#spotify
-
-flatpak install flathub com.spotify.Client -y
-
-#telegram
-
-#flatpak install flathub org.telegram.desktop -y
-
-### MINION for ESO and WOW addons
-
-flatpak install flathub gg.minion.Minion -y
-
-### ProtonUp-QT - for easy install of proton-GE
-
-flatpak install flathub net.davidotek.pupgui2 -y
-
-#install BRAVE
-
-flatpak install flathub com.brave.Browser -y
-
-#install FLOORP
-
-flatpak install flathub one.ablaze.floorp -y
+#Discord
+flatpak install flathub com.discordapp.Discord -y
 
 #install FLATSEAL - for flatpak permissions
 
@@ -58,6 +36,80 @@ flatpak install flathub com.github.tchx84.Flatseal -y
 #install WAREHOUSE - manage FLATPAKs
 
 #flatpak install flathub io.github.flattool.Warehouse -y
+
+#VSCode
+flatpak install flathub com.visualstudio.code -y
+
+#Thunderbird Mail client
+flatpak install flathub org.mozilla.Thunderbird -y
+
+#Audacity
+flatpak install flathub org.audacityteam.Audacity -y
+
+#Skype
+flatpak install flathub com.skype.Client -y
+
+#EasyEffects
+flatpak install flathub com.github.wwmm.easyeffects -y
+
+#YouTube Downloader
+flatpak install flathub io.github.aandrew_me.ytdn -y
+
+#Popsicle
+flatpak install flathub com.system76.Popsicle -y
+
+#spotify
+
+flatpak install flathub com.spotify.Client -y
+
+#telegram
+
+#flatpak install flathub org.telegram.desktop -y
+
+########### GAMING ##################
+
+#Steam
+flatpak install flathub com.valvesoftware.Steam -y
+
+#Lutris
+flatpak install flathub net.lutris.Lutris -y
+
+#Heroic Games Launcher
+flatpak install flathub com.heroicgameslauncher.hgl -y
+
+### MINION for ESO and WOW addons
+
+flatpak install flathub gg.minion.Minion -y
+
+### ProtonUp-QT - for easy install of proton-GE
+
+flatpak install flathub net.davidotek.pupgui2 -y
+
+
+########## BROWSERS ############
+
+#install BRAVE
+
+flatpak install flathub com.brave.Browser -y
+
+#install FLOORP
+
+flatpak install flathub one.ablaze.floorp -y
+
+#Microsoft EDGE Browser
+flatpak install flathub com.microsoft.Edge -y
+
+#Google Chrome Browser
+flatpak install flathub com.google.Chrome -y
+
+#Chromium Browser
+flatpak install flathub org.chromium.Chromium -y
+
+#Vivaldi Browser
+flatpak install flathub com.vivaldi.Vivaldi -y
+
+#Opera Browser
+flatpak install flathub com.opera.Opera -y
 
 #update All the FLATPAKS
 
